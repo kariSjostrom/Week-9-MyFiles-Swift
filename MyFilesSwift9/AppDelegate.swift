3a1e7cbc464b416ac0c9f3ac3a9b65a8d5fef99c
kariSjostrom <s0472138@student.rockvalleycollege.edu>
2015-10-26 15:34:48 -0500

    Changing name

--- MyFilesSwift9/AppDelegate.swift
+++ MyFilesSwift9/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  MyFilesSwift9
 //
-//  Created by Charles Konkol on 3/16/15.
+//  Created by Karissa Sjostrom on 10/26/15
 //  Copyright (c) 2015 Rock Valley College. All rights reserved.
 //
 
